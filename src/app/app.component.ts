70c892e9bbb5c393aada719ca8448381fc2d7ce4
mohammadghaliah <m.ghaliah98@gmail.com>
2022-08-14 20:54:44 +0300

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,8 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  toDate: Date = new Date();
+  title: string = 'Angular pipes Example';
+  num: number = 9542.14554;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TempConverterPipe } from './temp-converte.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, TempConverterPipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
